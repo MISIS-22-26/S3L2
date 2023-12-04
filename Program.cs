3f0cb71e99222a56f410ffd57fa7b7f982863551
Daniil Grigoryevich <danilanosikov@yandex.ru>
2023-12-04 16:39:39 +0000

    Added Simple Test

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Tree{
+	public class Program {
+		static void Main(){
+			
+			int[] testing_list = {226, 53, 226, 333, 335, 333};
+			var tree = new BinaryTree<int>(new(226));
+			tree.Append(testing_list);
+			tree.Print();
+		}
+	}
+}
